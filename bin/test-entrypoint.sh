c22fd394804e814323d8c3a7ef40f38394a8d5a7
Brian Grant <brian.grant@urbanairship.com>
2018-04-23 18:55:39 -0700

    revert last change

--- bin/test-entrypoint.sh
+++ bin/test-entrypoint.sh
@@ -4,14 +4,14 @@ export PATH=$PATH:~/.local/bin
 
 set -e
 
-# export PGPASSWORD=$POSTGRES_PASSWORD
-# until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d postgres -c '\q'
-# do
-#   >&2 echo "Postgres is unavailable - sleeping"
-#   sleep 5
-# done
-#
-# >&2 echo "Postgres is up"
+export PGPASSWORD=$POSTGRES_PASSWORD
+until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d postgres -c '\q'
+do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
+>&2 echo "Postgres is up"
 # echo Debug: $DEBUG
 
 # Collect static files
@@ -19,6 +19,5 @@ set -e
 # echo "Migrate"
 # ./manage.py migrate
 
-sleep 5
 
 ./manage.py test --nomigrations
